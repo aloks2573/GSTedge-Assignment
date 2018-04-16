6e0592dcf3430c68613f56cf185539e4db4b1bd7
Alok Singh <aloks2573@gmail.com>
2018-04-16 18:37:01 +0530

    updated the main.js file

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ for (var i = 0; i < 10; i++)
 (function(i)
 {
 setTimeout(function(){console.log(i);},1000);
-})(i)};
+})
+(i)};
+
+// Since I have called a function under a closure which scopes the variable in the closure only. Hence, in this case the value of I will be different 
+// for each closure and when the function defined in the setTimeOut would be called, it will be called with different values of i,which will print the values from 0 to 9.
